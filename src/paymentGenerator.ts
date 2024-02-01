bf9e2c60b79df29ad2297a862ac7a69bc1c79b22
Simonas Narbutas <simonas@narbuto.lt>
2024-02-01 14:01:10 +0200

    PM-1135: paymentGenerator run indefinitely

--- src/paymentGenerator.ts
+++ src/paymentGenerator.ts
@@ -7,8 +7,27 @@ export async function paymentGenerator(
     noTransactions: number,
     timeDelayMS: number
 ) {
+    if (noTransactions === -1) {
+        while (true) {
+            const receiver = receivers[Math.floor(Math.random() * receivers.length)];
+            await processTransaction(network, deployerAccount, receiver, timeDelayMS);
+        }
+    } else {
+        for (let i = 0; i < noTransactions; i++) {
+            const receiver = receivers[Math.floor(Math.random() * receivers.length)];
+            await processTransaction(network, deployerAccount, receiver, timeDelayMS);
+        }
+    }
+}
+async function processTransaction(
+    network: string,
+    deployerAccount: string,
+    receiver: string,
+    timeDelayMS: number
+) {
     const client = new Client({ network: 'testnet' });
     let sender_public = client.derivePublicKey(deployerAccount)
+    console.log("receiver: ", receiver);
     const query = `query MyQuery {
         account(publicKey: "${sender_public}") {
           inferredNonce
@@ -24,32 +43,28 @@ export async function paymentGenerator(
         }),
     })
     let inferred_nonce = parseInt((await response.json()).data.account.inferredNonce)
-    for (let i = 0; i < noTransactions; i++) {
-        const receiver = receivers[Math.floor(Math.random() * receivers.length)]
-        console.log("receiver: ", receiver)
-        let signedPayment = client.signPayment(
-            {
-                to: receiver,
-                from: sender_public,
-                amount: 1500000,
-                fee: 2000000000,
-                nonce: inferred_nonce + i
-            },
-            deployerAccount
-        );
-        const query_pay = `mutation MyMutation {
-            sendPayment(input: {fee: 2000000000,  amount: "1500000", to: "${receiver}", from: "${sender_public}", nonce: "${inferred_nonce + i}"}, signature: {field: "${signedPayment.signature.field}", scalar: "${signedPayment.signature.scalar}"})}`
-        await fetch(network, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                query: query_pay
-            }),
-        })
-            .then(r => r.json())
-            .then(data => console.log("data returned:", data))
-        await new Promise(r => setTimeout(r, timeDelayMS));
-    }
+    let signedPayment = client.signPayment(
+        {
+            to: receiver,
+            from: sender_public,
+            amount: 1500000,
+            fee: 2000000000,
+            nonce: inferred_nonce
+        },
+        deployerAccount
+    );
+    const query_pay = `mutation MyMutation {
+        sendPayment(input: {fee: 2000000000,  amount: "1500000", to: "${receiver}", from: "${sender_public}", nonce: "${inferred_nonce}"}, signature: {field: "${signedPayment.signature.field}", scalar: "${signedPayment.signature.scalar}"})}`;
+    await fetch(network, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            query: query_pay
+        }),
+    })
+        .then(r => r.json())
+        .then(data => console.log("data returned:", data));
+    await new Promise(r => setTimeout(r, timeDelayMS));
 }
