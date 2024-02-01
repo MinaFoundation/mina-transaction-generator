91fa2e3fda146aa91ccc6fa9df8157e82cc1fc39
Simonas Narbutas <simonas@narbuto.lt>
2024-02-01 15:02:51 +0200

    PM-1135: zkAppGenerator run indefinitely

--- src/zkAppGenerator.ts
+++ src/zkAppGenerator.ts
@@ -9,8 +9,27 @@ import {
 export async function zkAppGenerator(
     network: string,
     deployerAccount: string,
-    walletList: string[],
+    receivers: string[],
     noTransactions: number,
+    timeDelayMS: number
+) {
+    if (noTransactions === -1) {
+        while (true) {
+            const receiver = receivers[Math.floor(Math.random() * receivers.length)];
+            await processZKTransaction(network, deployerAccount, receiver, timeDelayMS);
+        }
+    } else {
+        for (let i = 0; i < noTransactions; i++) {
+            const receiver = receivers[Math.floor(Math.random() * receivers.length)];
+            await processZKTransaction(network, deployerAccount, receiver, timeDelayMS);
+        }
+    }
+}
+
+async function processZKTransaction(
+    network: string,
+    deployerAccount: string,
+    receiver: string,
     timeDelayMS: number
 ) {
     const devNet = Mina.Network(
@@ -18,7 +37,7 @@ export async function zkAppGenerator(
     );
     Mina.setActiveInstance(devNet);
 
-    const deployerPrivKey = PrivateKey.fromBase58(deployerAccount); //
+    const deployerPrivKey = PrivateKey.fromBase58(deployerAccount);
     const deployerPubKey = deployerPrivKey.toPublicKey();
     console.log('Sender public key:', deployerPubKey.toBase58().toString());
     const query = `query MyQuery {
@@ -37,34 +56,31 @@ export async function zkAppGenerator(
     })
     let inferred_nonce = parseInt((await response.json()).data.account.inferredNonce)
 
-    for (let i = 0; i < noTransactions; i++) {
-        let receiver = walletList[Math.floor(Math.random() * walletList.length)]
-        const toUserPublicKey = PublicKey.fromBase58(receiver);
-        console.log('Receiver public key:', toUserPublicKey.toBase58().toString());
+    const toUserPublicKey = PublicKey.fromBase58(receiver);
+    console.log('Receiver public key:', toUserPublicKey.toBase58().toString());
 
-        const MINA = 1e9;
-        const amount = 1 * MINA;
+    const MINA = 1e9;
+    const amount = 1 * MINA;
 
-        let memo = 'Test ZKApp to Receiver';
-        const transactionFee = 2 * MINA;
+    let memo = 'Test ZKApp to Receiver';
+    const transactionFee = 2 * MINA;
 
-        console.log('amount:', amount);
-        const tx = await Mina.transaction({ sender: deployerPubKey, fee: transactionFee, memo: memo, nonce: inferred_nonce + i }, () => {
-            let accountUpdate;
-            accountUpdate = AccountUpdate.createSigned(deployerPubKey);
-            accountUpdate.send({ to: toUserPublicKey, amount: UInt64.from(amount) });
-        });
-        await tx.prove();
-        console.log('tx.toPretty() : ' + JSON.stringify(tx.toPretty()));
+    console.log('amount:', amount);
+    const tx = await Mina.transaction({ sender: deployerPubKey, fee: transactionFee, memo: memo, nonce: inferred_nonce }, () => {
+        let accountUpdate;
+        accountUpdate = AccountUpdate.createSigned(deployerPubKey);
+        accountUpdate.send({ to: toUserPublicKey, amount: UInt64.from(amount) });
+    });
+    await tx.prove();
+    console.log('tx.toPretty() : ' + JSON.stringify(tx.toPretty()));
 
-        const res = await tx.sign([deployerPrivKey]).send();
+    const res = await tx.sign([deployerPrivKey]).send();
 
-        console.log('res : ' + JSON.stringify(res));
-        const hash = await res.hash();
+    console.log('res : ' + JSON.stringify(res));
+    const hash = await res.hash();
 
-        if (hash == null) {
-            console.log('error sending transaction (see above)');
-        }
-        await new Promise(r => setTimeout(r, timeDelayMS));
+    if (hash == null) {
+        console.log('error sending transaction (see above)');
     }
+    await new Promise(r => setTimeout(r, timeDelayMS));
 }
